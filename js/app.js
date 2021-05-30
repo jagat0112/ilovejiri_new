a6a6edeefe7518f95990977c65e865bd28d0a2a9
Jagat Pradhan <jagat.pradhan0112@gmail.com>
2021-05-30 19:31:46 +0545

    open graph updated

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+import lightGallery from "lightgallery.js";
+
 const collabsible = document.querySelectorAll(".collapsible");
 
 collabsible.forEach((c) => {
   c.addEventListener("click", function () {
     this.classList.toggle("collapsible--active");
   });
+});
+
+lightGallery(document.getElementById("footer-gallery__items"), {
+  selector: ".lightgallery",
 });
